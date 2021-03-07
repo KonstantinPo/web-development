ec01cedd2f022bb537355a8856a9e072a8b54406
Pk <pososhnov@gmail.com>
2021-03-07 20:27:06 +0300

    Лаба 3.5

--- lw3/task4/index.php
+++ lw3/task4/index.php
@@ -1,67 +1,47 @@
 <?php
 header("Content-Type: text/plain");
 $path = "data";
-$mail = Get("email");
-if ($mail === null) {
+$mail = getParameter("email");
+if ($mail === null) 
+{
 	echo "Параметр email не найден";
-} else {
-	
-	$mail = strtolower($mail);
-	if (!ValidateEmail($mail)) {
+} 
+else 
+{
+	if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) 
+	{
 		echo "Параметр email задан не верно";
-	} else {
-		$firstName = Get("first_name");
-		$lastName = Get("last_name");
-		$age = Get("age");
-		if ($age !== null && !ctype_digit($age)) {
+	} 
+	else 
+	{
+		$firstName = getParameter("first_name");
+		$lastName = getParameter("last_name");
+		$age = getParameter("age");
+		if ($age !== null && !ctype_digit($age))
+		{
 			echo "Параметр возраст задан неверно";
 			return;
 		}
-		if (!file_exists($path)) {
+		if (!file_exists($path))
+		{
 			mkdir($path);
 		}
 		SaveEntry($path, $mail, (string)$firstName, (string)$lastName, (string)$age);
-		echo "Данные:".PHP_EOL;
-		echo "email = ".$mail.PHP_EOL;
-		echo "Имя = ".$firstName.PHP_EOL;
-		echo "Фамилия = ".$lastName.PHP_EOL;
-		echo "Возраст = ".$age.PHP_EOL;
+		echo "Данные:" . PHP_EOL;
+		echo "email = " . $mail . PHP_EOL;
+		echo "Имя = " . $firstName . PHP_EOL;
+		echo "Фамилия = " . $lastName . PHP_EOL;
+		echo "Возраст = " . $age . PHP_EOL;
 		echo "Анкета сохранена";
 	}
 }
 
-function Get(string $key) : ?string {
-	return array_key_exists($key, $_GET) ? (string)$_GET[$key] : null;
+function getParameter(string $key) : ?string
+{
+	return isset($_GET[$key]) ? (string)$_GET[$key] : null;
 }
 
-function ValidateEmail(string $mail) {
-	$findDot = false;
-	$findAt = false;
-	$indexDot = 0;
-	$indexAt = 0;
-	$len = strlen($mail);
-	for ($i = 0; $i < $len; ++$i) {
-		$ch = $mail[$i];
-		if ($ch == "@") {
-			$indexAt = $i;
-			if ($indexAt == 0 || $indexAt == $len - 1) {
-				return false;
-			}
-			$findAt = true;
-		} else if ($ch == ".") {
-			$indexDot = $i;
-			if ($indexDot == 0 || $indexDot == $len - 1) {
-				return false;
-			}
-			$findDot = true;
-		}
-	}
-	return $findDot && $findAt && ($indexDot - $indexAt) > 1;
-}
-
-function SaveEntry(string $path, string $mail, string $firstName, string $lastName, string $age) {
-	file_put_contents($path."/".$mail.".txt", 
-		"first_name=".$firstName.PHP_EOL.
-		"last_name=".$lastName.PHP_EOL.
-		"age=".$age);
+function SaveEntry(string $path, string $mail, string $firstName, string $lastName, string $age) 
+{
+	file_put_contents($path . "/" . $mail . ".txt", "first_name=" . $firstName . PHP_EOL . "last_name=" . $lastName . PHP_EOL . "age=" . $age);
 }
---
+++ lw3/task5/index.php
@@ -0,0 +1,73 @@
+<?php
+header("Content-Type: text/plain");
+$path = "../task4/data";
+$mail = getParameter("email");
+if ($mail === null) 
+{
+	echo "Параметр email не найден";
+} 
+else 
+{
+	if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) 
+	{
+		echo "Параметр email задан не верно";
+	} 
+	else 
+	{
+		$firstName = "";
+		$lastName = "";
+		$age = "";
+		$errorMessage = "";
+		if (!GetEntry($path, $mail, $firstName, $lastName, $age, $errorMessage))
+		{
+			echo $errorMessage;
+		}
+		else
+		{
+			echo "Данные:" . PHP_EOL;
+			echo "email = " . $mail . PHP_EOL;
+			echo "Имя = " . $firstName . PHP_EOL;
+			echo "Фамилия = " . $lastName . PHP_EOL;
+			echo "Возраст = " . $age . PHP_EOL;
+		}
+		
+	}
+}
+
+function getParameter(string $key) : ?string
+{
+	return isset($_GET[$key]) ? (string)$_GET[$key] : null;
+}
+
+function GetEntry(string $path, string $mail, string& $firstName, string& $lastName, string& $age, string& $errorMessage) : bool
+{
+	$fileName = "$path/$mail.txt";
+	if (!file_exists($fileName))
+	{
+		$errorMessage = "Не найден файл: $fileName" . PHP_EOL;
+		return false;
+	}
+	$patternFirstName = "first_name=";
+	$patternLastName = "last_name=";
+	$patternAge = "age=";
+	$firstName = " ";
+	$lastName = " ";
+	$age = " ";
+	foreach (file($fileName) as $line)
+	{
+		$pos = strpos($line, $patternFirstName);
+		 if ($pos !== false)
+		 {
+			 $firstName = trim(substr($line , $pos + strlen($patternFirstName)));
+		 }
+		 else if (($pos = strpos($line, $patternLastName)) !== false)
+		 {
+			 $lastName = trim(substr($line , $pos + strlen($patternLastName)));
+		 }
+		 else if (($pos = strpos($line, $patternAge)) !== false)
+		 {
+			  $age = trim(substr($line , $pos + strlen($patternAge)));
+		 }
+	}
+	return true;
+}
