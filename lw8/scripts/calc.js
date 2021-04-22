220c9746ab2297f5d385c4585898812ec6a293a5
Konstantin Pososhnov <pososhnov@nata-info.ru>
2021-04-22 17:50:08 +0300

    +Лабораторная работа №8. Проверка на простые числа

--- lw8/scripts/calc.js
+++ lw8/scripts/calc.js
@@ -1,24 +1,24 @@
 function calc() {
     let polishNotation = document.getElementById('polishNotationString').value;
     if (polishNotation !== '') {
-        document.getElementById('result').innerHTML = calcImpl(polishNotation);
+        document.getElementById('polishNotationResult').innerHTML = calcImpl(polishNotation);
     }
 }
 
 function calcImpl(polishNotationString) {
-    let lexems = Parse(polishNotationString);
-    isConverted = true;    
-    while (lexems.length >= 3 && isConverted) {
-        isConverted = false;
+    let lexems = parse(polishNotationString);
+    let isExecuted = true;    
+    while (lexems.length >= 3 && isExecuted) {
+        isExecuted = false;
         polishNotationString = '';
-        i = 0;
+        let i = 0;
         while (i < lexems.length - 2) {
-            res = TryExecute(lexems[i], lexems[i + 1], lexems[i + 2]);
+            let res = tryExecute(lexems[i], lexems[i + 1], lexems[i + 2]);
             if (!res.result) {
                 return res.value;
             }
             if (res.value !== null) {
-                isConverted = true;
+                isExecuted = true;
                 polishNotationString += res.value + ' ';
                 i += 3;
                 continue;
@@ -29,35 +29,35 @@ function calcImpl(polishNotationString) {
         for ( ; i < lexems.length; i++) {
             polishNotationString += lexems[i] + ' ';
         }
-        if (isConverted) {
-            lexems = polishNotationString.split(' ').filter(function(e) {return e.length != 0});
+        if (isExecuted) {
+            lexems = polishNotationString.split(' ').filter(function(e) { return e.length != 0 });
         }
 
     }
-    return Number(polishNotationString) != NaN ? polishNotationString : 'Некорректная строка в польской нотации';    
+    return !isNaN(Number(polishNotationString)) ? polishNotationString : 'Некорректная строка в польской нотации';    
 }
 
-function Parse(string) {
+function parse(string) {
     return string.split(' ').filter(function(e) {return e.length != 0});
 }
 
-function TryExecute(operator, operand1, operand2) {
+function tryExecute(operator, operand1, operand2) {
     let res;
     switch(operator) {
         case '+':
-            res = Add(operand1, operand2);
+            res = add(operand1, operand2);
             break;
         case '-':
             res = Sub(operand1, operand2);
             break;
         case '*':
-            res = Mul(operand1, operand2);
+            res = mul(operand1, operand2);
             break;
         case '/':
-            res = Div(operand1, operand2);
+            res = div(operand1, operand2);
             break;
         default:
-            return CheckOperator(operator);
+            return checkOperator(operator);
     }
     return {
         result: true,
@@ -65,40 +65,36 @@ function TryExecute(operator, operand1, operand2) {
     };
 }
 
-function CheckOperator(operator) {
-    if (!isNaN(operator)) {
-        return {
-            result: true,
-            value: null,
-        }
-    } else {
-        return {
-            result: false,
-            value: 'Некорректный операнд в строке',
-        }
-    }
+function checkOperator(operator) {
+    return !isNaN(operator) ? {
+        result: true,
+        value: null,
+    } : {
+        result: false,
+        value: 'Некорректный операнд в строке',
+    };
 }
-function Add(str1, str2) {
+function add(str1, str2) {
     let term1 = Number(str1);
     let term2 = Number(str2);
-    return term1 !== NaN && term2 !== NaN ? term1 + term2 : null;
+    return !isNaN(term1) && !isNaN(term2) ? term1 + term2 : null;
 }
 
-function Sub(str1, str2) {
+function sub(str1, str2) {
     let sub1 = Number(str1);
     let sub2 = Number(str2);
-    return sub1 !== NaN && sub2 !== NaN ? sub1 - sub2 : null;
+    return !isNaN(sub1) && !isNaN(sub2) ? sub1 - sub2 : null;
 }
 
-function Mul(str1, str2) {
+function mul(str1, str2) {
     let mul1 = Number(str1);
     let mul2 = Number(str2);
-    return mul1 !== NaN && mul2 !== NaN ? mul1 * mul2 : null;
+    return !isNaN(mul1) && !isNaN(mul2) ? mul1 * mul2 : null;
    
 }
 
-function Div(str1, str2) {
+function div(str1, str2) {
     let dividend = Number(str1);
     let divisor = Number(str2);
-    return dividend !== NaN && divisor !== NaN ? dividend / divisor : null;
+    return !isNaN(dividend) && !isNaN(divisor) ? dividend / divisor : null;
 }
